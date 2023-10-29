79a1e6847cbd3f2d4f88ae0244f08a1ddd13d81d
Seung-wa Akpan <victorakpan169@gmail.com>
2023-10-29 22:16:01 +0100

    first commit

--- README.md
+++ README.md
@@ -599,4 +599,5 @@ This project was made possible by:
 [javascript]: https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1C
 [node]: https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white
 [express]: https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB
-[mongodb]: https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white
+[mongodb]: https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white#   b l o g - a p i  
+ 
--- index.js
+++ index.js
@@ -1,57 +1,59 @@
-const express = require('express')
-const app = express()
-const db = require('./db')
-const passport = require('passport')
-const userRoute = require('./routes/user.route')
-const blogRoute = require('./routes/blog.route')
-const os = require('os')
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const db = require("./db");
+const passport = require("passport");
+const logger = require("morgan");
+const userRoute = require("./routes/user.route");
+const blogRoute = require("./routes/blog.route");
+const os = require("os");
+const PORT = process.env.PORT || 3000;
 
 //CONNECT TO MONGOOSE
 if (process.env.NODE_ENV != "test") {
-    db.connectToDb()
+  db.connectToDb();
 }
-
 
 //Signup and login authentication middleware
-require('./authentication/auth')
+require("./authentication/auth");
 
 //To parse url encoded data
-app.use(express.urlencoded( {extended: false} ))
+app.use(express.urlencoded({ extended: false }));
+
+// logger
+app.use(logger("dev"));
 
 //To parse data passed via body
-app.use(express.json())
-
+app.use(express.json());
 
 //USER ROUTE
-app.use('/api', userRoute)
+app.use("/api", userRoute);
 
 //BLOG ROUTE
-app.use('/api/blog', blogRoute)
+app.use("/api/blog", blogRoute);
 
-app.get('/', (req, res) => {
-    res.status(200).json({status: true, message: `Welcome to Annies Blog API`})
-})
-
+app.get("/", (req, res) => {
+  res.status(200).json({ status: true, message: `Welcome to Annies Blog API` });
+});
 
 //Error Middleware function
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    
-    //send the first line of an error message 
-    if (err instanceof Error) return res.json({error: err.message.split(os.EOL)[0]})
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
 
-    res.json({ error: err.message });
-})
+  //send the first line of an error message
+  if (err instanceof Error)
+    return res.json({ error: err.message.split(os.EOL)[0] });
 
-app.use('*', (req, res) => {
-    res.status(404).json({status: false, message: `Route not found`})
-})
+  res.json({ error: err.message });
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({ status: false, message: `Route not found` });
+});
 
 if (process.env.NODE_ENV != "test") {
-    app.listen(PORT, () => {
-        console.log(`Server is running at PORT http://localhost:${PORT}`)
-    })
+  app.listen(PORT, () => {
+    console.log(`Server is running at PORT http://localhost:${PORT}`);
+  });
 }
 
-module.exports = app
+module.exports = app;
